e0f205daf20e9f5d45f15159c469720421c84db6
MushroomMuncher67 <khayliidapogi@gmail.com>
2025-12-19 10:59:11 +0000

    test

---
+++ Experiment4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a = 5, b = 3;
+
+    cout << "Sum: " << a + b << endl;
+    cout << "Product: " << a * b;
+    return 0;
+}
---
+++ Experiment8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string first = "C++ ";
+    string second = "Programming";
+
+    cout << first + second;
+    return 0;
+}
